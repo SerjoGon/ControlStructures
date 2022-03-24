f21b98862491f61f815fa1a1cf0a331c4787a479
SerjoGon <mr.serhioru@mail.ru>
2022-03-24 20:12:47 +0300

    Shift massive

--- Arrays/Arrays/Main.cpp
+++ Arrays/Arrays/Main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 void main()
 {
 	setlocale(LC_ALL, "Russian");
-	int a = 0, b = 0;
+	double sum = 0, average = 0;
 	const int n = 5; // кол-во элем массива
 	int arr[n] = {};
 	cout << "Введите элементы массива (" << n << "шт)";
@@ -24,13 +24,22 @@ void main()
 	for (int i = n - 1; i >= 0; i--)	cout << arr[i] << "\t";
 	cout << endl;
 
-	for (int i = 0; i < n; i++) a += arr[i];
-	cout << "Сумма элементов массива:" << a << endl;
-	b = a / n;
-	cout << "Среднее арифметическое элементов массива:" << b << endl;
+	for (int i = 0; i < n; i++) sum += arr[i];
+	cout << "Сумма элементов массива:" << sum << endl;
+	average = sum / n;
+	cout << "Среднее арифметическое элементов массива:" << average << endl;
 
 	cout << "Сравнение элементов массива:" << endl;
+	int min, max;
+	min = max = arr[0];
 	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] < min) min = arr[i];
+		if (arr[i] > max) max = arr[i];
+	}
+	cout << "Mininal value  " << min << endl;
+	cout << "Maximal value  " << max << endl;
+	/*for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < n; j++)
 		{
@@ -38,5 +47,5 @@ void main()
 			cout << arr[i] << " \t" << arr[j] << "\t";
 		}
 		cout << endl;
-	}
+	}*/
 }
