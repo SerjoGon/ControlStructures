79809d582ffa1bf2e4af6397dc62101e2b1722b3
SerjoGon <mr.serhioru@mail.ru>
2022-03-27 19:06:54 +0300

    home works

---
+++ Arrays/Octal/main.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+	int dec = 0;
+	const int size = 32;
+	int oct[size] = {};
+	cout << "Введите десятичное число: "; cin >> dec;
+	int i = 0;
+	for (; dec;i++)
+	{
+		oct[i] = dec % 8;
+		dec /= 8;
+	}
+
+	cout << "Восьмеричное число: ";
+	for (i--; i >= 0;i--)
+	{
+		cout << oct[i];
+	}
+
+}
--- Arrays/Shift/main.cpp
+++ Arrays/Shift/main.cpp
@@ -7,16 +7,25 @@ void main()
 	const int n = 10;
 	int arr[n] = { 1,2,3,4,5,6,7,8,9,10 };
 	// вывод исходного массива на экран
-	for (int i = 0; i < n; i++)
+	/*for (int i = 0; i < n; i++)
 	{
 		cout << arr[i] << "\t";
 	}
-	cout << endl;
+	cout << endl;*/
 
 	// Циклический сдвиг массива
 	int number_of_shifts;
 	cout << "введите количество сдвигов ";cin >> number_of_shifts;
 	for (int i = 0; i < number_of_shifts; i++)
+	{
+		int buffer = arr[n - 1];
+		for (int i = n -1; i >= 0; i--)
+		{
+			arr[i] = arr[i-1];
+		}
+		arr[0] = buffer;
+	}
+	/*for (int i = 0; i < number_of_shifts; i++)
 	{
 		int buffer = arr[0];
 		for (int i = 0; i < n; i++)
@@ -25,7 +34,7 @@ void main()
 		}
 		arr[n - 1] = buffer;
 
-	}
+	}*/
 	// вывод сдвинутого массива на экран
 
 	for (int i = 0; i < n; i++)
