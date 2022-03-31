b61acf7c0683a80bc9f929ac10ec1c490b8a8f0b
SerjoGon <mr.serhioru@mail.ru>
2022-03-31 19:36:21 +0300

    UNIQUE Massive

--- Arrays/Hexadecimal/HEX.cpp
+++ Arrays/Hexadecimal/HEX.cpp
@@ -12,21 +12,13 @@ void main()
 	for (; dec; i++)
 	{
 		hex[i] = dec % 16;
-		/*switch (dec % 16)
-		{
-		case 10: hex[i] = 'A';break;
-		case 11: hex[i] = 'B';break;
-		case 12: hex[i] = 'C';break;
-		case 13: hex[i] = 'D';break;
-		case 14: hex[i] = 'E';break;
-		case 15: hex[i] = 'F';break;
-		}*/
 		dec /= 16;
 	}
 	cout << "Шестнадцатеричное число: ";
 	for (i--;i >= 0;i--)
 	{
-			switch (hex[i])
+		cout << (hex[i] = 10 ? char(65) : hex[i] = 11 ? char(66) : hex[i] = 12 ? char(67) : hex[i] = 13 ? char(68) : hex[i] = 14 ? char(69) : hex[i] = 15 ? char(70):hex[i]);
+			/*switch (hex[i])
 			{
 			case 10: cout << "A";break;
 			case 11: cout << "B";break;
@@ -35,7 +27,7 @@ void main()
 			case 14: cout << "E";break;
 			case 15: cout << "F";break;
 			default:cout << hex[i];
-			}
+			}*/
 		/*if (hex[i] >= 10)
 		{
 			switch (hex[i])
--- Arrays/UniqueRand/main.cpp
+++ Arrays/UniqueRand/main.cpp
@@ -1,11 +1,12 @@
 ﻿#include<iostream>
 using namespace std;
 #define tab "\t"
+//#define UNIQUE
 void main()
 {
 	setlocale(LC_ALL, "");
 
-	const int n = 10;
+	/*const int n = 10;
 	int arr[n] = {};
 	for (int i = 0; i < n; i++)
 	{
@@ -16,12 +17,16 @@ void main()
 		cout << arr[i] << tab;
 	}
 	cout << endl;
-	
+
 	for (int i = 0; i < n; i++)
 	{
-		for (int j = i + 1; j < n;j++)
+		for (int j = 0; j < i;j++)
 		{
-			if (arr[i] == arr[j]) arr[j] = rand() % n;
+			if (arr[i] == arr[j])
+			{
+				arr[i] = rand() % n;
+				j = -1;
+			}
 		}
 	}
 	cout << endl;
@@ -30,6 +35,55 @@ void main()
 	{
 		cout << arr[i] << tab;
 	}
+	cout << endl;*/
+
+	const int n = 10;
+	int arr[n] = {};
+	bool unique;
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % n;
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << tab;
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % n;
+		for (int j = 0; j < i; j++)
+		{
+			if (arr[i] == arr[j])
+			{
+				i--;
+				break;
+			}
+		}
+	}
+
+#ifdef UNIQUE
+	for (int i = 0; i < n; i++)
+	{
+		do
+		{
+			arr[i] = rand() % n;
+			unique = true;
+			for (int j = 0; j < i; j++)
+			{
+				if (arr[i] == arr[j])
+				{
+					unique = false;
+					break;
+				}
+	}
+} while (!unique);
+	}
+#endif
 	cout << endl;
-	
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << tab;
+	}
 }
