f90085a1531232b32c09c371dca9aabf7baddf39
SerjoGon <mr.serhioru@mail.ru>
2022-05-23 15:01:43 +0300

    Fraction homework

--- Introduction in to OOP/Fraction/main.cpp
+++ Introduction in to OOP/Fraction/main.cpp
@@ -88,6 +88,17 @@ public:
 		INT++;
 		return old;
 	}
+	Fraction& operator--()
+	{
+		INT--;
+		return *this;
+	}
+	Fraction operator--(int)
+	{
+		Fraction old = *this;
+		INT--;
+		return old;
+	}
 	Fraction& operator=(const Fraction& other)
 	{
 
@@ -97,6 +108,21 @@ public:
 		cout << "CopyAssignment:\t" << this << endl;
 		return *this;
 	}
+	/*Fraction& operator-=(Fraction other)
+	{
+		int INT = other.INT;
+		int num = other.num;
+		int den = other.den;
+		this->num = multiplier(this->den, den,this->den);
+		this->den = multiplier(this->den, den,this->den);
+		num = multiplier(this->den, den,den);
+		den = multiplier(this->den, den,den);
+		this->INT -= other.INT;
+		this->num -= num;
+		this->den = den;
+		return *this;
+
+	}*/
 	// Methods
 
 	void to_proper()// переводит в правильную дробь(выделяет целую часть )
@@ -109,7 +135,6 @@ public:
 		num += INT * den;
 		INT = 0;
 	}
-
 	void print()const
 	{
 		/*if (INT) cout << INT;
@@ -124,7 +149,58 @@ public:
 		else cout << INT << "(" << num << "/" << den << ")" << endl;
 	}
 };
+//Greates common divisor - НОД - наибольший общий делитель (из переводчика гугл)
+int GCD(int l_den, int r_den)
+{
+	if (l_den % r_den == 0) return r_den;
+	if (r_den % l_den == 0) return l_den;
+	if (l_den > r_den) 
+		return GCD(l_den % r_den, r_den);
+	return GCD(l_den, r_den % l_den);
+}
+// least common multiplier - НОК - наименьший общий множитель
+int LCM(int l_den, int r_den)
+{
+	return (l_den * r_den) / GCD(l_den, r_den);
+}
+//Функция выделения множителя дроби
+int multiplier(int l_den, int r_den, int den)
+{
+	return LCM(l_den, r_den) / den;
+}
 
+Fraction operator+(Fraction& left, Fraction& right)
+{
+	Fraction result;
+	Fraction fract_1;
+	Fraction fract_2;
+	int l_den = left.get_den();
+	int r_den = right.get_den();
+	fract_1.set_num(left.get_num() * multiplier(l_den, r_den, l_den));
+	fract_1.set_den(left.get_den() * multiplier(l_den, r_den, l_den));
+	fract_2.set_num(left.get_num() * multiplier(l_den, r_den, r_den));
+	fract_2.set_den(left.get_den() * multiplier(l_den, r_den, r_den));
+	result.set_INT(left.get_INT() + right.get_INT());
+	result.set_num(fract_1.get_num() + fract_2.get_num());
+	result.set_den(fract_1.get_den());
+	return result;
+}
+Fraction operator-(Fraction& left, Fraction& right)
+{
+	Fraction result;
+	Fraction fract_1;
+	Fraction fract_2;
+	int l_den = left.get_den();
+	int r_den = right.get_den();
+	fract_1.set_num(left.get_num() * multiplier(l_den, r_den, l_den));
+	fract_1.set_den(left.get_den() * multiplier(l_den, r_den, l_den));
+	fract_2.set_num(left.get_num() * multiplier(l_den, r_den, r_den));
+	fract_2.set_den(left.get_den() * multiplier(l_den, r_den, r_den));
+	result.set_INT(left.get_INT() - right.get_INT());
+	result.set_num(fract_1.get_num() - fract_2.get_num());
+	result.set_den(fract_1.get_den());
+	return result;
+}
 Fraction operator*( Fraction left, Fraction right)
 {
 	left.to_improper();
@@ -145,12 +221,21 @@ Fraction operator*( Fraction left, Fraction right)
 		left.get_den() * right.get_den()
 	);
 }
+Fraction operator/(Fraction left, Fraction right)
+{
+	left.to_improper();
+	right.to_improper();
+	return Fraction
+	(
+		left.get_num() * right.get_den(),
+		left.get_den() * right.get_num()
+	);
+}
 
 void main()
 {
 	setlocale(LC_ALL, "RU");
-	int INT = 0, num = 0, den = 0;
-	while (true)
+	/*while (true)
 	{
 		cout << "Введите простую дробь! ";cin >> INT >> num >> den;
 		if (den == 0) cout << "\n Делить на 0 нельзя! Введите новую дробь!!! \n";
@@ -165,8 +250,11 @@ void main()
 		else
 			break;
 	}
-	Fraction B(INT, num, den);
-	
+	Fraction B(INT, num, den);*/
+	Fraction A(1,2,3);
+	Fraction B(2,3,4);
+	A.to_improper();
+	B.to_improper();
 	A.print();
 	B.print();
 	Fraction C = A * B;
@@ -180,5 +268,9 @@ void main()
 	Fraction E = B;
 	E.print();
 #endif // CHECK
+	Fraction D = C / A;
+	D.print();
+	Fraction M = C + D;
+	M.print();
 
 }
