00f04e07322f3e02d4dbc0a168c531d15cc16767
SerjoGon <mr.serhioru@mail.ru>
2022-06-13 22:03:58 +0300

    Output in Files

---
+++ Inheritance/Files/main.cpp
@@ -0,0 +1,18 @@
+﻿#include<iostream>
+#include<fstream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	std::ofstream fout; // создаем поток
+	fout.open("File.txt", std::ios_base::app);// открываем поток
+	fout << "\t\t\tHello World!\n";// выводим в поток
+	fout.close();// закрываем поток
+	// потоки обязательно нужно закрывать, это так же важно как удалять памят
+	// сколько раз был вызван open(), столько же раз нужно вызвать метод close();
+	system("notepad File.txt");
+	return 0;
+}
