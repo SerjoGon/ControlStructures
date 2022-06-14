b7c70b12b34f099aa210555ebfc7d6a74e7604f9
SerjoGon <mr.serhioru@mail.ru>
2022-06-14 19:38:33 +0300

    Files rename done

--- Inheritance/Files/main.cpp
+++ Inheritance/Files/main.cpp
@@ -1,5 +1,6 @@
 ﻿#include<iostream>
 #include<fstream>
+#include<Windows.h>
 using std::cin;
 using std::cout;
 using std::endl;
@@ -7,12 +8,16 @@ using std::endl;
 int main()
 {
 	setlocale(LC_ALL, "ru");
+
+	const int size = 256;
+	char str[size] = {};
+	cin.getline(str, size);
 	std::ofstream fout; // создаем поток
-	fout.open("File.txt", std::ios_base::app);// открываем поток
+	fout.open(str, std::ios_base::app);// открываем поток
 	fout << "\t\t\tHello World!\n";// выводим в поток
 	fout.close();// закрываем поток
 	// потоки обязательно нужно закрывать, это так же важно как удалять памят
 	// сколько раз был вызван open(), столько же раз нужно вызвать метод close();
-	system("notepad File.txt");
+	//system("notepad");
 	return 0;
 }
