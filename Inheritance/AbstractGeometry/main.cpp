1ecdea4d218527a2f4186b87d1c2c468adfb74ff
SerjoGon <mr.serhioru@mail.ru>
2022-06-29 09:59:43 +0300

    RectangularTriangle Done

--- Inheritance/AbstractGeometry/main.cpp
+++ Inheritance/AbstractGeometry/main.cpp
@@ -3,13 +3,13 @@
 #include<Windows.h>
 #include<wingdi.h>
 #include<math.h>
-using std::cout; 
+using std::cout;
 using std::cin;
 using std::endl;
 
 namespace Geometry
 {
-	
+
 #define IDM_ELLIPSE		1100
 	//enum (Enumeration - перечисление) - это набор целочисленных констант.
 // Перечесление так же является типом данных.
@@ -58,7 +58,7 @@ namespace Geometry
 			else if (start_x > Defaults::max_start_x)this->start_x = Defaults::max_start_x;
 			else this->start_x = start_x;
 		}
-		void set_start_y(int start_y) 
+		void set_start_y(int start_y)
 		{
 			if (start_y < Defaults::min_start_y)this->start_y = Defaults::min_start_y;
 			else if (start_y > Defaults::max_start_y)this->start_y = Defaults::max_start_y;
@@ -70,7 +70,7 @@ namespace Geometry
 			else if (line_width > Defaults::max_line_width)this->line_width = Defaults::max_line_width;
 			else this->line_width = line_width;
 		}
-		Shape(int start_x,int start_y,unsigned int line_width, Color color) :color(color) 
+		Shape(int start_x, int start_y, unsigned int line_width, Color color) :color(color)
 		{
 			set_start_x(start_x);
 			set_start_y(start_y);
@@ -92,9 +92,9 @@ namespace Geometry
 	class Triangle :public Shape
 	{
 	public:
-		Triangle(int start_x, int start_y, unsigned int line_width, Color color):
-			Shape(start_x, start_y, line_width, color){}
-		~Triangle(){}
+		Triangle(int start_x, int start_y, unsigned int line_width, Color color) :
+			Shape(start_x, start_y, line_width, color) {}
+		~Triangle() {}
 		virtual double get_height()const = 0;
 	};
 	class EquilateralTriangle :public Triangle
@@ -113,7 +113,7 @@ namespace Geometry
 		{
 			set_side(side);
 		}
-		~EquilateralTriangle(){}
+		~EquilateralTriangle() {}
 		double get_height()const
 		{
 			return sqrt(pow(side, 2) - pow(side / 2, 2));
@@ -134,17 +134,17 @@ namespace Geometry
 			HBRUSH hBrush = CreateSolidBrush((WORD)color);
 			SelectObject(hdc, hPen);
 			SelectObject(hdc, hBrush);
-			POINT vertices[] = 
+			POINT vertices[] =
 			{
 				{start_x, start_y + side },
-				{start_x+side,start_y+side},
-				{start_x+side/2,start_y+side-get_height()}
+				{start_x + side,start_y + side},
+				{start_x + side / 2,start_y + side - get_height()}
 			};
-			::Polygon(hdc,vertices,sizeof(vertices)/sizeof(vertices[0]));
+			::Polygon(hdc, vertices, sizeof(vertices) / sizeof(vertices[0]));
 			DeleteObject(hPen);
 			DeleteObject(hBrush);
 
-			ReleaseDC(hwnd,hdc);
+			ReleaseDC(hwnd, hdc);
 		}
 		void info()const
 		{
@@ -155,14 +155,14 @@ namespace Geometry
 		}
 	};
 
-	class IsoscalesTriangle :public Triangle
+	class IsoscelesTriangle :public Triangle
 	{
 		double base;
 		double side;
 	public:
 		double get_base()const { return base; }
 		double get_side()const { return side; }
-		void set_base(double base) 
+		void set_base(double base)
 		{
 			if (base < Defaults::min_line_length)base = Defaults::min_line_length;
 			else if (base > Defaults::max_line_length)base = Defaults::max_line_length;
@@ -176,19 +176,19 @@ namespace Geometry
 			if (side * 2 <= base)side = base;
 			this->side = side;
 		}
-		IsoscalesTriangle& operator()(double base, double side)
+		IsoscelesTriangle& operator()(double base, double side)
 		{
 			if (base >= side * 2)base = side;
 			set_base(base);
 			set_side(side);
 			return *this;
 		}
-		IsoscalesTriangle(double base, double side, int start_x, int start_y, unsigned int line_width, Color color) :
+		IsoscelesTriangle(double base, double side, int start_x, int start_y, unsigned int line_width, Color color) :
 			Triangle(start_x, start_y, line_width, color)
 		{
 			operator()(base, side);
 		}
-		~IsoscalesTriangle(){}
+		~IsoscelesTriangle() {}
 		double get_height()const
 		{
 			return sqrt(pow(side, 2) - pow(base / 2, 2));
@@ -227,6 +227,72 @@ namespace Geometry
 			cout << "Основание треугольника: " << base << endl;
 			cout << "Сторона треугольника: " << side << endl;
 			cout << "Высота треугольника: " << get_height() << endl;
+			Shape::info();
+		}
+	};
+	class RectangularTriangle :public Triangle
+	{
+		double base;
+		double side;
+	public:
+		double get_base()const { return base; }
+		double get_side()const { return side; }
+		void set_base(double base)
+		{
+			if (base < Defaults::min_line_length)this->base = Defaults::min_line_length;
+			else if (base > Defaults::max_line_length) this->base = Defaults::max_line_length;
+			else this->base = base;
+		}
+		void set_side(double side)
+		{
+			if (side < Defaults::min_line_length)this->side = Defaults::min_line_length;
+			else if (side > Defaults::max_line_length) this->side = Defaults::max_line_length;
+			else this->side = side;
+		}
+		RectangularTriangle(double base, double side, int start_x, int start_y, unsigned int line_width, Color color) :
+			Triangle(start_x, start_y, line_width, color)
+		{
+			set_base(base);
+			set_side(side);
+		}
+		~RectangularTriangle(){}
+		double get_height()const
+		{
+			return side;
+		}
+		double get_area()const
+		{
+			return (base * side) / 2;
+		}
+		double get_perimeter()const
+		{
+			return sqrt(pow(base, 2) + pow(side, 2)) + base + side;
+		}
+		void draw()const
+		{
+			HWND hwnd = GetConsoleWindow();
+			HDC hdc = GetDC(hwnd);
+			HPEN hPen = CreatePen(PS_SOLID, line_width, 0x00FF00FF);
+			HBRUSH hBrush = CreateSolidBrush((WORD)color);
+			SelectObject(hdc, hPen);
+			SelectObject(hdc, hBrush);
+			POINT vertices[] =
+			{
+				{start_x, start_y },
+				{start_x ,start_y + side},
+				{start_x + base ,start_y }
+			};
+			::Polygon(hdc, vertices, sizeof(vertices) / sizeof(vertices[0]));
+			DeleteObject(hPen);
+			DeleteObject(hBrush);
+
+			ReleaseDC(hwnd, hdc);
+		}
+		void info()const
+		{
+			cout << typeid(*this).name() << endl;
+			cout << "Основание треугольника(катет А): " << base << endl;
+			cout << "Высота треугольника(Катет В): " << get_height() << endl;
 			Shape::info();
 		}
 	};
@@ -287,7 +353,7 @@ namespace Geometry
 			if (side_b <= 0)side_b = 10;
 			this->side_b = side_b;
 		}
-		Rectangle(double side_a, double side_b, int start_x, int start_y, unsigned int line_width, Color color) 
+		Rectangle(double side_a, double side_b, int start_x, int start_y, unsigned int line_width, Color color)
 			:Shape(start_x, start_y, line_width, color)
 		{
 			set_side_a(side_a);
@@ -319,7 +385,7 @@ namespace Geometry
 			SelectObject(hdc, hPen);
 			SelectObject(hdc, hBrush);
 			//5) Рисуем прямоугольник
-			::Rectangle(hdc, start_x, start_y, start_x+side_a, start_y+side_b);
+			::Rectangle(hdc, start_x, start_y, start_x + side_a, start_y + side_b);
 			DeleteObject(hPen);
 			DeleteObject(hBrush);
 			ReleaseDC(hwnd, hdc);
@@ -338,7 +404,7 @@ namespace Geometry
 		double radius;
 	public:
 		double get_radius()const { return radius; }
-		void set_radius(double radius) 
+		void set_radius(double radius)
 		{
 			if (radius >= 10 && radius <= 500) this->radius = radius;
 			else if (radius < 10)this->radius = 10;
@@ -349,7 +415,7 @@ namespace Geometry
 		{
 			set_radius(radius);
 		}
-		~Circle(){}
+		~Circle() {}
 		double get_area()const { return (radius * radius) * M_PI; }
 		double get_diameter()const { return radius * 2; }
 		double get_perimeter()const { return get_diameter() * M_PI; }
@@ -368,7 +434,7 @@ namespace Geometry
 			HBRUSH hBrush = CreateSolidBrush((WORD)color);
 			SelectObject(hdc, hPen);
 			SelectObject(hdc, hBrush);
-			::Ellipse(hdc, start_x, start_y, start_x + radius*2, start_y+radius*2);
+			::Ellipse(hdc, start_x, start_y, start_x + radius * 2, start_y + radius * 2);
 			DeleteObject(hPen);
 			DeleteObject(hBrush);
 			ReleaseDC(hwnd, hdc);
@@ -391,8 +457,10 @@ int main()
 	circle.info();
 	Geometry::EquilateralTriangle qtri(200, 190, 300, 5, Geometry::Color::green);
 	qtri.info();*/
-	Geometry::IsoscalesTriangle itri(100, 150, 200, 220, 8, Geometry::Color::red);
-	itri.info();
+	/*Geometry::IsoscelesTriangle itri(100, 150, 200, 220, 8, Geometry::Color::red);
+	itri.info();*/
+	Geometry::RectangularTriangle rtrg(120, 180, 200, 220, 8, Geometry::Color::yellow);
+	rtrg.info();
 	//Нижняя граница 
 	return 0;
 }
